11ee915bd9b07b90ad4eca7b8c69bb178a4d8e83
Federico N Fernandez <federico.nagual1@gmail.com>
2025-07-19 20:13:27 -0300

    add: 2025-07-19

---
+++ SegundaEntrega/EquipamientoInfraestructura.java
@@ -0,0 +1,24 @@
+
+
+/**
+ * @author gero1
+ * @version 1.0
+ * @created 19-jul-2025 00:05:50
+ */
+public class EquipamientoInfraestructura {
+
+	private char denominacion;
+	private char descripcion;
+	private date fechaIncorporacion;
+	private hour montoInvertido;
+
+// Código de set y get
+
+%list="Attribute" @separator="
+
+" @indent="	" template="JavaGetterSetter"%
+
+	public EquipamientoInfraestructura(){
+
+	}
+}//end EquipamientoInfraestructura
---
+++ SegundaEntrega/GrupoUTN.h
@@ -0,0 +1,33 @@
+/****************************************************
+ *  GrupoUTN.h                                         
+ *  Created on: 19-jul-2025 00:06:38                      
+ *  Implementation of the Class GrupoUTN       
+ *  Original author: gero1                     
+ ****************************************************/
+
+#if !defined(EA_D27EA417_5607_4fd9_842F_C838A80A2CF2__INCLUDED_)
+#define EA_D27EA417_5607_4fd9_842F_C838A80A2CF2__INCLUDED_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "Personal.java"
+#include "FacuRegional.h"
+#include "EquipamientoInfraestructura.java"
+#include "DocumentacionBiblioteca.java"
+
+Personal *m_Personal;
+FacuRegional *m_FacuRegional;
+EquipamientoInfraestructura *m_EquipamientoInfraestructura;
+DocumentacionBiblioteca *m_DocumentacionBiblioteca;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!defined(EA_D27EA417_5607_4fd9_842F_C838A80A2CF2__INCLUDED_)*/
+ 
